8cf69936841aa66f24d98ef499bffa469e4a2e5b
Rafael Madakis <rafaelmadakis@bol.com.br>
2021-05-06 16:00:57 -0300

    JWT final

--- src/main/java/com/rafael/auth/config/SecurityConfig.java
+++ src/main/java/com/rafael/auth/config/SecurityConfig.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.rafael.auth.jwt.JwtConfigurer;
 import com.rafael.auth.jwt.JwtTokenProvider;
 
 @Configuration
@@ -19,10 +23,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 
 	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		return passwordEncoder;
+	}
+
+	@Bean
 	@Override
 	public AuthenticationManager authenticationManager() throws Exception {
 		return super.authenticationManagerBean();
 
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+			
+		http
+		.httpBasic().disable()
+		.csrf().disable()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and()
+			.authorizeRequests()
+			.antMatchers("/login").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.apply( new JwtConfigurer(jwtTokenProvider));
 	}
 
 }
